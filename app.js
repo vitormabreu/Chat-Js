ea2480a97292d8c144fa37b56583b907ddb89a26
Vitor Abreu <vitinabreu@gmail.com>
2017-06-18 12:53:31 -0300

    Server side finished foe now

--- app.js
+++ app.js
@@ -11,11 +11,29 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-//SocketIO
+//SocketIO evento de conexão
 io.on("connection", function (client) {
-	console.log('usuário conectado');
+	client.on("join", function(name){
+		console.log("Joined: " + name);
+		clients[client.id] = name;
+		client.emit("update", "Você acaba de se conectar ao chat.");
+		client.broadcast.emit("update", name + " entrou no chat.")
+	});
+
+	//SocketIO evento de envio de mensagens
+	client.on("send", function(msg){
+		console.log("Message: " + msg);
+		client.broadcast.emit("chat", clients[client.id], msg);
+	});
+	
+	//SocketIO evento de desconexão
+	client.on("disconnect", function(){
+		console.log("Disconnect");
+		io.emit("update", clients[client.id] + " saiu da sala.");
+		delete clients[client.id];
+	});
 });
 
 http.listen(4200, function(){
-	console.log('listening on port 4200');
+	console.log('server escutando na porta 4200');
 });
